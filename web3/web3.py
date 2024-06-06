6444299fef26c2248ba06115ab9fd7d6da4996c8
Vinícius dos Santos Alves <mtafg567@gmail.com>
2024-06-05 23:26:52 -0300

    Wallet and ETH account creation and trades included

--- web3/web3.py
+++
@@ -1,9 +0,0 @@
-from web3 import Web3, geth_poa_middleware
-
-RPC_URL = 'https://sepolia.infura.io/v3/'
-
-
-class Web3Service:
-    def __init__(self):
-        self.w3 = Web3(Web3.HTTPProvider(RPC_URL))
-        self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
